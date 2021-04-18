d187d5edb0a869cee74434fc4a9317b3c7d1741c
Bobb-93 <borisiakimov@gmail.com>
2021-04-18 15:38:45 +0300

    one XHR function, less repetitivness

--- front-end/indexOneFunction.js
+++ front-end/indexOneFunction.js
@@ -1,8 +1,20 @@
 // YOUR CODE HERE
 const pictures = {
-    picture1: document.querySelector(".pictures-container>img[data-id='1']"),
-    picture2: document.querySelector(".pictures-container>img[data-id='2']"),
-    picture3: document.querySelector(".pictures-container>img[data-id='3']")
+    picture1: 
+        {
+            selector: document.querySelector(".pictures-container>img[data-id='1']"),
+            relativePath: "../back-end/lyrics/charlotte_sometimes.txt"
+        },
+    picture2: 
+        {
+            selector: document.querySelector(".pictures-container>img[data-id='2']"),
+            relativePath: "../back-end/lyrics/hey_you.txt"
+        },
+    picture3: 
+        {
+            selector: document.querySelector(".pictures-container>img[data-id='3']"),
+            relativePath: "../back-end/lyrics/pictures_of_you.txt"
+        }
 };
 
 const output = {
@@ -12,39 +24,36 @@ const output = {
 
 let lyricsURL = "";
 
-// console.log(pictures.picture1);
+// console.log(pictures);
+for (const picture in pictures) { 
+    // console.log(`${picture}: ${pictures[picture]}`);
 
-//lyrics of first song
-function loadLyrics(chosenPicture) {
-    let xhttp = new XMLHttpRequest();
-    console.log(chosenPicture);
-    xhttp.open("GET", chosenPicture, true);
+    pictures[picture].selector.addEventListener("click", function(){
+        let xhttp = new XMLHttpRequest();
+        // console.log(chosenPicture);
+        xhttp.open("GET", pictures[picture].relativePath);
 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            output.songLyrics.innerHTML = this.responseText;
-        }
-    };
-    xhttp.send();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                output.songLyrics.innerHTML = this.responseText;
+            }
+        };
+        xhttp.send();
+    });
 }
 
-// console.log(pictures);
-// for (const picture in pictures) {
-//     picture.addEventListener("click", loadLyrics);
-//     console.log(picture);
-// }
+//Междинен вариант, но с повторения:
+// pictures.picture1.addEventListener('click', function(){
+//     lyricsURL = "../back-end/lyrics/charlotte_sometimes.txt";
+//     loadLyrics(lyricsURL);
+// });
 
-pictures.picture1.addEventListener('click', function(){
-    lyricsURL = "../back-end/lyrics/charlotte_sometimes.txt";
-    loadLyrics(lyricsURL);
-});
+// pictures.picture2.addEventListener('click', function(){
+//     lyricsURL = "../back-end/lyrics/hey_you.txt";
+//     loadLyrics(lyricsURL);
+// });
 
-pictures.picture2.addEventListener('click', function(){
-    lyricsURL = "../back-end/lyrics/hey_you.txt";
-    loadLyrics(lyricsURL);
-});
-
-pictures.picture3.addEventListener('click', function(){
-    lyricsURL = "../back-end/lyrics/pictures_of_you.txt";
-    loadLyrics(lyricsURL);
-});
+// pictures.picture3.addEventListener('click', function(){
+//     lyricsURL = "../back-end/lyrics/pictures_of_you.txt";
+//     loadLyrics(lyricsURL);
+// });
